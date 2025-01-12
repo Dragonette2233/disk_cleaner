85814a34130ba953d012ec95538e6e95b4cbfa24
DragonettE <rodanban251@gmail.com>
2025-01-13 00:22:30 +0300

    tooltip smart adapted

--- smart_check.py
+++ smart_check.py
@@ -22,11 +22,8 @@ def get_short_smarts():
         stdout, _ = process.communicate()  # Use communicate() to read stdout and stderr
         strings = stdout.split('\n')
 
-        smart_end = []
-        smart_long = stdout.split("Vendor Specific SMART Attributes with Thresholds:")
-        if len(smart_long) > 1:
-            
-            smarts_complex.append(smart_long[1])
+        smart_short_view = []
+        smart_complex_view = []
         # print(strings)
         for i in strings:
             i_as_l = i.split()
@@ -34,16 +31,22 @@ def get_short_smarts():
                 if i_as_l[0] in ('1', '4', '5', '9', '197', '198'):
                     match i_as_l[0]:
                         case '5':
-                            smart_end.append(i_as_l[-1])
+                            smart_short_view.append(i_as_l[-1])
+                            smart_complex_view.append(f"Relocated -- {i_as_l[-1]}")
                         case '197':
-                            smart_end.append('p' + i_as_l[-1])
+                            smart_short_view.append('p' + i_as_l[-1])
+                            smart_complex_view.append(f"Current pending -- {i_as_l[-1]}")
                         case '198':
-                            smart_end.append('u' + i_as_l[-1])
+                            smart_short_view.append('u' + i_as_l[-1])
+                            smart_complex_view.append(f"Offline uncorrectable -- {i_as_l[-1]}")
+                        case '9':
+                            smart_complex_view.insert(0, f"Power on hours -- {i_as_l[-1]}")
             
-                # print('--'.join([i_as_l[0], i_as_l[1], i_as_l[-1]]))
                         
-        short_smart = ''.join(i for i in smart_end)
-        
-        smarts_short.append(short_smart)
+        short_string = ''.join(i for i in smart_short_view)
+        complex_string = '\n'.join(i for i in smart_complex_view)
+   
+        smarts_short.append(short_string)
+        smarts_complex.append(complex_string)
     
     return [smarts_short, smarts_complex]
